80e95e2f65d5b4863d7162d8a371876418889665
Vladislav Ermolaev <vermoouth@ya.ru>
2023-05-15 18:10:54 +0300

    Minor refactoring + additional log

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ const utils = require('./utils');
 const getDependecyURLs = function (packageNames) {
 	console.log('Creating temporary project');
 	const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'depsave-'));
+
 	console.log(`Operating in ${tempDir}`);
 	fs.writeFileSync(path.join(tempDir, 'package.json'), '{"private": true}');
+
 	console.log('Creating lock file for temporary project');
 	install = child_process.execFileSync(
 		'npm',
 		[
-			'install', 
-			'--package-lock-only', 
-			'--quiet', 
+			'install',
+			'--package-lock-only',
+			'--quiet',
 			'--no-audit',
 			...packageNames
 		],
@@ -26,9 +28,12 @@ const getDependecyURLs = function (packageNames) {
 			cwd: tempDir, shell: true, stdio: 'inherit'
 		}
 	);
+
 	console.log(`Fetching dependencies' urls for ${packageNames.join(', ')}`)
 	const tempLock = require(path.join(tempDir, 'package-lock'));
 	urls = Object.values(tempLock.dependencies).map(dep => dep.resolved);
+
+	console.log(`Removing temporafy directory ${tempDir}`)
 	utils.rmdirRecursiveSync(tempDir);
 
 	return urls;
