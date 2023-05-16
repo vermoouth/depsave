04fa5748e019e1406d98f9b8325d091b1678c0e4
Vladislav Ermolaev <vermoouth@ya.ru>
2023-05-16 15:29:52 +0300

    Add dependency downloading functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const utils = require('./utils');
 
 
 const getDependecyURLs = function (packageNames) {
-	console.info(`Using temporary project to fetch links for ${packageNames.join(', ')} dependencies`)
+	console.info(`Using temporary project to fetch links for ${packageNames.join(', ')} dependencies`);
 	console.debug('Creating temporary project');
 	const tempDir = fs.mkdtempSync(path.join(os.tmpdir(), 'depsave-'));
 
@@ -26,18 +26,37 @@ const getDependecyURLs = function (packageNames) {
 			...packageNames
 		],
 		{
-			cwd: tempDir, shell: true, stdio: 'inherit'
+			cwd: tempDir,
+			shell: true,
+			stdio: 'inherit'
 		}
 	);
 
-	console.debug(`Fetching dependencies' urls for ${packageNames.join(', ')}`)
+	console.debug(`Fetching dependencies' urls for ${packageNames.join(', ')}`);
 	const tempLock = require(path.join(tempDir, 'package-lock'));
 	urls = Object.values(tempLock.dependencies).map(dep => dep.resolved);
 
-	console.debug(`Removing temporafy directory ${tempDir}`)
+	console.debug(`Removing temporafy directory ${tempDir}`);
 	utils.rmdirRecursiveSync(tempDir);
 
 	return urls;
 };
 
+
+const batchDownload = async function (urls, downloadDir) {
+	// TODO: create directory
+	urls.forEach(function (url, i) {
+		console.debug(`Fetching [ ${i + 1} / ${urls.length} ] - ${path.basename(url, path.extname(url))}`);
+		downloadFile(url, downloadDir);
+	})
+};
+
+
+const downloadFile = async function (url, downloadDir) {
+	const response = await fetch(url);
+	if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
+	response.body.pipe(fs.createWriteStream(path.join(downloadDir, path.basename(url))));
+};
+
 console.log(getDependecyURLs(['node-fetch@2', 'yaml']));
+// batchDownload(getDependecyURLs(['@vue/cli-service']), '../downloads');
