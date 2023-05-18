ec00a4f2f3755b27e5c0827cab8c1f1a0601b08c
Vladislav Ermolaev <vermoouth@ya.ru>
2023-05-18 16:31:20 +0300

    Prepare cli to accept package.json files

--- bin/cli.js
+++ bin/cli.js
@@ -2,20 +2,33 @@
 
 const cli = require('commander');
 
-const depsave = require('../lib/index');
+const { depsave } = require('../lib/index');
 const package = require('../package.json');
+
+
+function collect(val, memo) {
+  memo = memo === undefined ? [] : memo
+
+  memo.push(val);
+  return memo;
+}
 
 
 cli
   .name(package.name)
+  .description(package.description)
   .version(package.version.toString(), '-v, --version')
   .option('-d, --directory [path/to/directory]', 'custom directory for downloaded dependencies (default: depsave-<epoch_time>)')
+  .option('-f, --file [path/to/package.json]', 'package.json file to fetch dependencies from', collect)
   .option('-c, --concurrent [int]', 'maximum amount of concurrent downloads', 20)
   .arguments('[package-names...]')
   .action((packages) => {
+    if (packages.length && cli.file.length) {
+      console.warn('Usage of package names and package.json file simultaneously is not recommended yet')
+    };
     depsave(
       packages,
-      [],
+      cli.file,
       {
         directory: cli.directory,
         concurrent: cli.concurrent
