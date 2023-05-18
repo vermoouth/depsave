15c3b9988897ed91bc0c8c740336144b2504bfee
Vladislav Ermolaev <vermoouth@ya.ru>
2023-05-18 12:53:48 +0300

    Add usage section to readme, mior fixes usage section of cli

--- README.md
+++ README.md
@@ -5,8 +5,17 @@ npm approach.
 
 ## Installation
 ```bash
-npm install depsave
+npm i -g depsave
 ```
 
 
 ## Usage
+```
+Usage: depsave [options] [package-names...]
+
+Options:
+  -v, --version                     output the version number
+  -d --directory [directory]        custom directory for downloaded dependencies
+  -c --concurrent [max-concurrent]  maximum amount of concurrent downloads
+  -h, --help                        output usage information
+```
--- bin/cli.js
+++ bin/cli.js
@@ -9,8 +9,8 @@ const package = require('../package.json');
 cli
   .name(package.name)
   .version(package.version.toString(), '-v, --version')
-  .option('-d --directory [directory]', 'custom directory for downloaded dependencies')
-  .option('-c --concurrent [max-concurrent]', 'maximum amount of concurrent downloads')
+  .option('-d, --directory [directory]', 'custom directory for downloaded dependencies')
+  .option('-c, --concurrent [max-concurrent]', 'maximum amount of concurrent downloads')
   .arguments('[package-names...]')
   .action((packages) => {
     index.depsave(packages, [], {directory: cli.directory, concurrent: cli.concurrent});
